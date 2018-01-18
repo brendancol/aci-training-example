5d0379f3ff08626e3064d493e2ee98bf218834bf
bcollins <parietal.io@gmail.com>
2018-01-18 14:16:21 -0500

    added a test for show_tenants

--- aci/core.py
+++ aci/core.py
@@ -22,8 +22,27 @@ def login_into_aci(username, password, url):
     return True
 
 
-def show_tenants():
-    print('show_tenants')
+def show_tenants(object_tree):
+    print('.show_tenants')
+
+    if not isinstance(object_tree, dict):
+        msg = 'Please use an object tree dict'
+        raise ValueError(msg)
+
+    if not 'objects' in object_tree:
+        msg = 'Object Tree must contain objects key'
+        raise ValueError(msg)
+
+
+    objects = object_tree['objects']
+    tenants = []
+    for obj in objects:
+
+        # TODO: make sure that objects have `type` key
+        if obj['type'] == 'tenant':
+            tenants.append(obj)
+
+    return tenants
 
 
 def create_tenant():
--- aci/tests/test_core.py
+++ aci/tests/test_core.py
@@ -1,4 +1,6 @@
 from aci.core import login_into_aci
+from aci.core import show_tenants
+
 
 def test_login_into_aci():
     username = 'mock_username'
@@ -9,4 +11,36 @@ def test_login_into_aci():
 
     assert was_successful is not None
     assert was_successful is True
-    
+
+
+def test_show_tenants():
+    print('show_tenants')
+
+    # test inputs
+    tenant1 = {}
+    tenant1['type'] = 'tenant1'
+    tenant1['name'] = 'tenant_name1'
+    tenant1['description'] = 'The is the main tenant'
+    tenant1['vlan'] = 'tenant_vlan1'
+    tenant1['contracts'] = ['contract1', 'contract2']
+
+    tenant2 = {}
+    tenant2['type'] = 'tenant'
+    tenant2['name'] = 'tenant_name'
+    tenant1['description'] = 'The is another tenant'
+    tenant2['vlan'] = 'tenant_vlan'
+    tenant2['contracts'] = ['contract1', 'contract2']
+
+    object_tree = {}
+    object_tree['name'] = 'my_object_tree'
+    object_tree['objects'] = [tenant1, tenant2]
+
+    # test code
+    tenants = show_tenants(object_tree)
+
+    assert isinstance(tenants, list)
+    assert len(tenants)
+    for t in tenants:
+        assert isinstance(t, dict)
+        assert 'type' in t
+        assert t['type'] == 'tenant'
